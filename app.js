37dbd0c7e3815592183dd7fe1ba58731ffc2fd47
barry <berlogan35@gmail.com>
2021-07-19 14:08:34 +0100

    Secure access to MongoDB Atlas

--- app.js
+++ app.js
@@ -5,8 +5,7 @@ const app = express();
 const routes = require('./routes/article.routes');
 const mongoose = require('mongoose');
 const port = process.env.PORT || 5050;
-const atlas_uri = "mongodb+srv://1234:1234@microservices.rjvap.mongodb.net/Blog?retryWrites=true&w=majority"
-const mongodb_uri = atlas_uri || process.env.MONGO_URI;
+const mongodb_uri = MONGO_ATLAS || process.env.MONGO_URI;
 
 // Connect to MongoDB
 mongoose.connect(mongodb_uri,
